f946577a401fab20af548c8e759164bd713762cc
yanbo0039 <yanbo0039@yeah.net>
2023-01-27 19:13:01 +0800

    完成项目

---
+++ src/com/tencent/filecrud/AppointedLineNumberCRUD.java
@@ -0,0 +1,342 @@
+package com.tencent.filecrud;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * <p>Title: AppointedLineNumberCRUD</p>  
+ * <p>
+ *    Description: 想要实现 对 文本 的 
+ *    增 指定行插入行数据 
+ *    删 
+ *    改 指定行更改行数据 
+ *    查 统计文本字数
+ * </p>
+ * </p>  
+ * @author xianxian 
+ * @date 2023年1月27日
+ */
+public class AppointedLineNumberCRUD {
+
+	public static void main(String[] args) {
+//		try {
+//			setAppointedLineNumber(2, "我想要将第二行的 ff2 数据修改 ");
+//			addAppointedLineNumber(5, ""
+//					+ "2023/01/26\r\n"
+//					+ "睡眠\r\n"
+//					+ "1. 入睡\r\n"
+//					+ "2. 起床\r\n"
+//					+ "3. 深睡\r\n"
+//					+ "能量\r\n"
+//					+ "1. 早餐\r\n"
+//					+ "2. 午餐\r\n"
+//					+ "3. 晚餐\r\n"
+//					+ "4. 其它\r\n"
+//					+ "工作");
+
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+
+//		2. 完成AppointedLineNumberCRUD
+
+//		String content = ""
+//				+ "睡眠\r\n"
+//				+ "1. 入睡\r\n"
+//				+ "2. 起床\r\n"
+//				+ "3. 深睡\r\n"
+//				+ "能量\r\n"
+//				+ "1. 早餐\r\n"
+//				+ "2. 午餐\r\n"
+//				+ "3. 晚餐\r\n"
+//				+ "4. 其它\r\n"
+//				+ "工作";
+
+//		appendBybufferedWriter("1. mylucky", "D:\\alibaba\\studyLog\\study.log",true);
+//		appendBybufferedWriter("1. mylucky", null,true);
+//		System.out.println(readAppointedLineNumberByFileReader(-1,"D:\\alibaba\\studyLog\\study.log"));
+//		System.out.println(readAppointedLineNumberByFileReader(-1,null));
+
+//		   String text = "汉字汉字仮名かなカナ 0189 azAZ ./ ";
+//		     System.out.println(count(text));
+//		countNumOfWords("D:\\alibaba\\studyLog\\study.log");
+		countNumOfWords(null);
+		
+	}
+
+	/**
+	 * <p>Title: countNumOfWords</p>  
+	 * <p>
+	 *    Description: 统计文本的字数 
+	 *    可以细分到 汉字 多少 英文（大小写）多少 数据多少 其它字符多少 全文共多少字节
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param pathStr  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void countNumOfWords(String pathStr) {
+		try {// try代码块，当发生异常时会转到catch代码块中
+				// 读取指定的文件
+			if (pathStr == null || pathStr.equals("")) {
+				pathStr = System.getProperty("user.dir") + File.separator + "log.log";
+			}
+			// 创建类进行文件的读取，并指定编码格式为utf-8
+			InputStreamReader read = new InputStreamReader(new FileInputStream(pathStr), "utf-8");
+			BufferedReader in = new BufferedReader(read);// 可用于读取指定文件
+
+			String linDatastr = null;// 定义一个字符串类型变量linDatastr 用于存放一行的文本数据
+			String wordStr = null;// 定义一个字符串类型变量wordStr
+			int lineCount = 0, // 定义一个整型变量,用于统计行数
+					countWords = 0, // 定义整型变量，用于统计字符数
+					chineseWords = 0, upperWords = 0, lowerWords = 0, numberWords = 0, otherWords = 0,
+					allEnglishWords = 0, countBytes = 0;// 定义一个整型变量，用于统计字节数
+			while ((linDatastr = in.readLine()) != null) {// readLine()方法, 用于读取一行,只要读取内容不为空就一直执行
+				lineCount++;// 每循环一次就进行一次自增，用于统计文本行数
+				countWords += linDatastr.length();// 用于统计总字符数
+				byte[] bytes = linDatastr.getBytes();// 求出该行的字节数组
+				countBytes += bytes.length;// 用于统计总字节数
+				for (int j = 0; j < linDatastr.length(); j++) {// for循环的条件，当j小于该行长度时就一直循环并自增
+					wordStr = Character.toString(linDatastr.charAt(j));// 返回一个字符串对象
+					if (wordStr.matches("[\\u4e00-\\u9fa5]")) {// if语句的条件，判断是否为汉字
+						chineseWords++;// 若为汉字则c1自增
+					} else if (wordStr.matches("[A-Z]")) {// if语句的条件，判断是否为大写字母
+						upperWords++;// 若为大写字母则c2自增
+					} else if (wordStr.matches("[a-z]")) {// if语句的条件，判断是否为小写字母
+						lowerWords++;// 若为小写字母则c3自增
+					} else if (wordStr.matches("[0-9]")) {// if语句的条件，判断是否为数字
+						numberWords++;// 若为数字则c4自增
+					} else {// 否则可判断为其他字符
+						otherWords++;// 若为其他字符则c5自增
+					}
+				}
+			}
+			allEnglishWords = upperWords + lowerWords;// 统计总的字母数
+			in.close();// 关闭流
+			System.out.println("该文本共有" + lineCount + "行");// 输出总的行数
+			System.out.println("该文本共有" + countWords + "个字符");// 输出总的字符数
+			System.out.println("其中包含：");// 输出提示信息
+			System.out.println(chineseWords + "个汉字");// 输出汉字数
+			System.out.println(allEnglishWords + "个字母，其中" + upperWords + "个大写字母，" + lowerWords + "个小写字母");// 输出字母数
+			System.out.println(numberWords + "个数字");// 输出数字数
+			System.out.println(otherWords + "个其他字符");// 输出其它字符数
+			System.out.println("该文本共有" + countBytes + "个字节");// 输出总的字节数
+
+		} catch (IOException e) {// 当try代码块有异常时转到catch代码块
+			e.printStackTrace();// printStackTrace()方法是打印异常信息在程序中出错的位置及原因
+		}
+	}
+
+	/**
+	 * <p>Title: getFullTime</p>  
+	 * <p>
+	 *    Description: 获取当前时间
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @return  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static String getFullTime() {
+		// 日期当前时间
+		Date time = new Date();
+		// 格式化日期
+		SimpleDateFormat sdfTime = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		// 转换格式
+		return sdfTime.format(time);
+	}
+
+	/**
+	 * <p>Title: appendBybufferedWriter</p>  
+	 * <p>
+	 *    Description: 
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param msg
+	 * @param pathStr
+	 * @param onOffTime  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void appendBybufferedWriter(String msg, String pathStr, Boolean onOffTime) {
+		if (pathStr == null || pathStr.equals("")) {
+			pathStr = System.getProperty("user.dir") + File.separator + "log.log";
+		}
+		File file = new File(pathStr);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			// 参数true 表示是否在原文件内容后追加 若不写或写false 则覆盖原文
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(pathStr, true));
+			// 内容输出到指定文件中
+			if (onOffTime == null || onOffTime == false) {
+				bufferedWriter.append(msg + " ");
+			} else {
+				bufferedWriter.append(msg + " " + getFullTime());
+			}
+			bufferedWriter.newLine();
+			bufferedWriter.flush();
+			bufferedWriter.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * <p>Title: setOrAddAppointedLineNumberDataByFiles</p>  
+	 * <p>
+	 *    Description: 更新指定行数据 或 在指定行数据后面添加数据
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param lineNumber
+	 * @param data
+	 * @param setOrAdd   传值set 则为 更改指定行数据 传add 则在指定行后面添加数据
+	 * @param pathStr
+	 * @throws Exception  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	public static void setOrAddAppointedLineNumberDataByFiles(int lineNumber, String data, String setOrAdd,
+			String pathStr) throws Exception {
+		if (pathStr == null || pathStr.equals("")) {
+			pathStr = System.getProperty("user.dir") + File.separator + "log.log";
+		}
+		Path path = Paths.get(pathStr);
+		List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+		
+		
+		if (Math.abs(lineNumber) > lines.size()) {
+			throw new Exception("指定行【" + lineNumber + "】不在文件行数范围内");
+		}
+		if (lines.size() < Math.abs(lineNumber)) {
+			throw new Exception("指定列 lineNumber： " + lineNumber + "大于文件本身的行数： " + lines.size());
+		} else {
+			if (setOrAdd == null || setOrAdd.equals("")) {
+				throw new Exception("请传入正确的 setOrAdd 值");
+			} else {
+				if (setOrAdd.equals("set") || setOrAdd.equals("add")) {
+					if (setOrAdd.equals("set")) {
+						lines.set(lineNumber < 0 ? lines.size() + lineNumber : lineNumber - 1,data);
+					} else {
+						lines.add(lineNumber < 0 ? lines.size() + lineNumber + 1 : lineNumber,data);
+					}
+					Files.write(path, lines, StandardCharsets.UTF_8);
+				} else {
+					throw new Exception("请传入正确的 setOrAdd 值");
+				}
+			}
+		}
+	}
+
+	/**
+	 * <p>Title: readAppointedLineNumberByFileReader</p>  
+	 * <p>
+	 *    Description: 1.根据指定行读数据 -N 为 倒数第N行
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param lineNumber
+	 * @param pathStr
+	 * @return  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	public static String readAppointedLineNumberByFileReader(int lineNumber, String pathStr) {
+		String appointedLine = "";
+		FileReader in = null;
+		LineNumberReader reader = null;
+
+		if (pathStr == null || pathStr.equals("")) {
+			pathStr = System.getProperty("user.dir") + File.separator + "log.log";
+		}
+		try {
+			in = new FileReader(pathStr);
+			reader = new LineNumberReader(in);
+			long totalLine = Files.lines(Paths.get(pathStr)).count();
+			if (Math.abs(lineNumber) > totalLine) {
+				throw new Exception("指定行【" + lineNumber + "】不在文件行数范围内");
+			}
+
+			int line = 1;
+			reader.setLineNumber((int) (lineNumber < 0 ? totalLine + lineNumber + 1 : lineNumber));
+			long i = reader.getLineNumber();
+			String s = "";
+
+			while ((s = reader.readLine()) != null) {
+				if (i == line) {
+					appointedLine = s;
+					break;
+				}
+				line++;
+			}
+			return appointedLine;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeResource(in, reader);
+		}
+		return appointedLine;
+	}
+
+	/**
+	 * <p>Title: closeResource</p>  
+	 * <p>
+	 *    Description: 2.关闭资源
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param in
+	 * @param reader  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	public static void closeResource(FileReader in, LineNumberReader reader) {
+		try {
+			if (reader != null) {
+				reader.close();
+			}
+			if (in != null) {
+				in.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/tencent/filecrud/FileChange.java
@@ -0,0 +1,339 @@
+package com.tencent.filecrud;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * <p>Title: FileChange</p>  
+ * <p>
+ * Description: 对文件的操作 增：例写xxx.log文件或者.xsl文件里写内容 删：对指定文件夹及文件夹下所有的文件删除 改： 查:
+ * 合并去重功能：对两文件夹里面的内容进行合并 同名文件 分别移至对应的文件夹保存
+ * </p>  
+ * @author xianxian 
+ * @date 2023年1月15日
+ */
+public class FileChange {
+	public static void main(String[] args) {
+//		concatFiles("G:\\Java测试文件夹\\照片分类\\148开头文件\\1480751587817.jpg", 
+//				"G:\\Java测试文件夹\\照片分类\\新建文件夹\\1480751587817.jpg"); //合并文件
+
+//		try {
+//			copyDir("G:\\需要复制的文件\\Java测试文件夹\\照片分类\\148开头文件", "G:\\需要复制的文件\\Java测试文件夹\\照片分类\\新建文件夹");//复制文件
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+
+//		deleteDirectoryLegacyIO("G:\\需要复制的文件\\Java测试文件夹\\照片分类\\新建文件夹",false);
+		concatFiles("G:\\需要复制的文件\\Java测试文件夹\\照片分类\\148开头文件", "G:\\需要复制的文件\\Java测试文件夹\\测试合并文件", null, null, null, null);
+	}
+
+	/**
+	 * <p>Title: info</p>  
+	 * <p>
+	 *    Description: 定义记录日志的方法
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param msg
+	 * @param pathStr  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void info(String msg, String pathStr) {
+		if (pathStr == null || pathStr.equals("")) {
+			pathStr = System.getProperty("user.dir") + File.separator + "log.log";
+		}
+		File file = new File(pathStr);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				System.out.println(e.getMessage() + "error wuwuw ");
+				e.printStackTrace();
+			}
+		}
+		try {
+			// 参数true 表示是否在原文件内容后追加 若不写或写false 则覆盖原文
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(pathStr, true));
+			// 日期当前时间
+			Date time = new Date();
+			// 格式化日期
+			SimpleDateFormat sdfTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			// 转换格式
+			String strTime = sdfTime.format(time);
+			// 内容输出到指定文件中
+			bufferedWriter.append(msg + " " + strTime);
+			bufferedWriter.newLine();
+
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * <p>Title: deleteDirectoryLegacyIO</p>  
+	 * <p>
+	 *    Description: 删除文件夹下所有文件及文件夹
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param filePathStr
+	 * @param deletSelefPath 是否删除本身文件 ture 删除本身文件夹 false 不删除本身文件夹
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void deleteDirectoryLegacyIO(String filePathStr, Boolean deletSelef) {
+		if (filePathStr != null || filePathStr.equals("") != true) {
+			File file = new File(filePathStr);
+			File[] fileList = file.listFiles();// 无法做到list多层文件夹数据
+			if (fileList != null) {
+				for (File fileItem : fileList) {// 先去递归删除子文件夹及子文件
+					deleteDirectoryLegacyIO(fileItem.getAbsolutePath(), true);
+				}
+			}
+			// deletSelefPath 为true 则删除 自身文件夹
+			if (deletSelef) {
+				if (file.delete()) {// 再删除自己本身的文件夹
+					System.out.println("删除成功 : %s%n " + filePathStr);
+					info("删除成功 : %s%n " + filePathStr, null);
+				} else {
+					System.err.println("删除失败 : %s%n " + filePathStr);
+				}
+			}
+		} else {
+			System.err.println("file 不为能空");
+		}
+	}
+
+	/**
+	 * <p>Title: copyDir</p>  
+	 * <p>
+	 *    Description: 
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param oldPath
+	 * @param newPath
+	 * @throws IOException  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void copyDir(String oldPath, String newPath) throws IOException {
+		File oldFile = new File(oldPath);
+		// 文件名称列表
+		String[] oldFilePath = oldFile.list();
+
+		if (!(new File(newPath)).exists()) {
+			(new File(newPath)).mkdir();
+		}
+
+		for (int i = 0; i < oldFilePath.length; i++) {
+			if ((new File(oldPath + oldFile.separator + oldFilePath[i])).isDirectory()) {
+				System.out.println(new File(oldPath + oldFile.separator + oldFilePath[i]) + " 是个文件夹 需要重新调用一下方法");
+				info(new File(oldPath + oldFile.separator + oldFilePath[i]) + " 是个文件夹 需要重新调用一下方法",
+						newPath + oldFile.separator + "log.log");
+				copyDir(oldPath + oldFile.separator + oldFilePath[i], newPath + oldFile.separator + oldFilePath[i]);
+			}
+
+			if (new File(oldPath + oldFile.separator + oldFilePath[i]).isFile()) {
+				copyFile(oldPath + oldFile.separator + oldFilePath[i], newPath + oldFile.separator + oldFilePath[i]);
+				System.out.println(oldPath + oldFile.separator + oldFilePath[i] + "  复制到  " + newPath
+						+ oldFile.separator + oldFilePath[i] + " 成功 ");
+				info(oldPath + oldFile.separator + oldFilePath[i] + "  复制到  " + newPath + oldFile.separator
+						+ oldFilePath[i] + " 成功 ", newPath + oldFile.separator + "log.log");
+
+			}
+		}
+	}
+	/**
+	 * <p>Title: copyFile</p>  
+	 * <p>
+	 *    Description: 
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param oldPath
+	 * @param newPath
+	 * @throws IOException  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void copyFile(String oldPath, String newPath) throws IOException {
+		File oldFile = new File(oldPath);
+		File newFile = new File(newPath);
+		FileInputStream in = new FileInputStream(oldFile);
+		FileOutputStream out = new FileOutputStream(newFile);
+		byte[] buffer = new byte[2097152];
+		while ((in.read(buffer)) != -1) {
+			out.write(buffer);
+		}
+	}
+
+	/**
+	 * <p>Title: compareFileNameStr</p>  
+	 * <p>
+	 *    Description: 比较 fileNameStr 字符串 是否 包含 partFileNameStr 字符串
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param partFileNameStr 字符串
+	 * @param fileNameStr     文件名
+	 * @param headOrTail      从头或者尾截取 fileNameStr 字符串进行比较 从头传参为 0 尾部 为 -1
+	 * @return  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static Boolean compareFileNameStr(String partFileNameStr, String fileNameStr, int headOrTail) {
+		if (fileNameStr.length() > partFileNameStr.length()) {
+			if (headOrTail == 0) {// 如果为0则从开始截取字符串相比较
+				return fileNameStr.substring(0, partFileNameStr.length()).equals(partFileNameStr);
+			} else {// 否则从尾部截取比较
+				return fileNameStr.substring(fileNameStr.length() - partFileNameStr.length()).equals(partFileNameStr);
+			}
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * <p>Title: concatFiles</p>  
+	 * <p>
+	 * Description: 实现文件汇总到 destFileAll 文件夹 功能 例srcFile 文件夹 与 destFileAll 文件夹中
+	 * 有很多同名文件 该方法将 srcFile 文件夹合并到destFileAll文件夹中 同名文件 内存较小存放minDestFileAll文件夹
+	 * 内存相同存放theSameDestFileAll文件夹 内存较大的存放destFileAll文件夹 最终清空srcFile文件夹为目的
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param srcFile            需要清空的文件夹 必传参数
+	 * @param destFileAll        汇总的文件夹 必传参数
+	 * @param minDestFileAll     存放 同名文件质量较小 文件夹 可指定路径
+	 * @param theSameDestFileAll 存放 同名文件质量相等 文件夹 可指定路径
+	 * @param partFileNameStr    需要与文件名比对的字符 不需比对可以不传
+	 * @param headOrTail         从头或是尾部比对 只能为"0"或其它字符类型的数字 不需要比对也可不传
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void concatFiles(String... strings) {
+		for (int i = 0; i < strings.length; i++) {
+			String srcFileStr = strings[0], destFileAllStr = strings[1], minDestFileAll = strings[2],
+					theSameDestFileAll = strings[3], partFileNameStr = strings[4], headOrTail = strings[5];
+			File srcFile = new File(srcFileStr), destFileAll = new File(destFileAllStr);
+			File[] srcFileArray = srcFile.listFiles(), destFileAllArray = destFileAll.listFiles();
+			// 如果 minDestFileAll theSameDestFileAll 未专值 则取 destFileAll 的兄弟级目录创建对应的 文件夹
+			if (minDestFileAll == null || minDestFileAll.equals("")) {
+				minDestFileAll = destFileAll.getParent() + File.separator + "同名文件质量较小";
+			}
+			if (theSameDestFileAll == null || theSameDestFileAll.equals("")) {
+				theSameDestFileAll = destFileAll.getParent() + File.separator + "同名文件质量相等";
+			}
+
+			if (!new File(minDestFileAll).exists()) {
+				new File(minDestFileAll).mkdirs();
+			}
+
+			if (!new File(theSameDestFileAll).exists()) {
+				new File(theSameDestFileAll).mkdirs();
+			}
+
+			for (File srcFile_iteam : srcFileArray) {
+				// srcFile 文件夹中的 某文件 是否 与 destFileAll 文件夹中 所有文件 有相同的命名 默认没有相当命名即没有重复
+				Boolean srcFileIteamTheSameAsAllDestFillIteam = false;
+				Boolean compareFileNameStrResult = partFileNameStr == null || partFileNameStr.equals("") ? true
+						: compareFileNameStr(partFileNameStr, srcFile_iteam.getName(), Integer.parseInt(headOrTail));
+				if (srcFile_iteam.isFile() && compareFileNameStrResult) {
+					// 将 srcFile文件夹中的元素 与 destFileAll文件夹中的每一个元素 相比较
+					for (File destFileAll_iteam : destFileAllArray) {// 如果目标文件为空进不了这个循环
+						if (destFileAll_iteam.isFile()) {
+//							1. 如果 srcFile文件夹中元素 与 与 destFileAll文件夹中的任意一个元素 名字相同 则 比较 它们的大小
+							if (srcFile_iteam.getName().equals(destFileAll_iteam.getName())) {
+								srcFileIteamTheSameAsAllDestFillIteam = true;// 名字相同了 表示有复复的了 更改为true
+								if (srcFile_iteam.length() > destFileAll_iteam.length()) {
+									// destFileAll 元素应该先移动至 minDestFileAll 同名文件质量较小 文件夹
+									// 如果 srcFile 元素比 destFileAll 元素大 则 将大的元素 移动至 destFileAll 文件夹
+									moveFile(destFileAll_iteam.getAbsolutePath(), minDestFileAll);
+									moveFile(srcFile_iteam.getAbsolutePath(), destFileAll_iteam.getParent());
+									System.out.println(destFileAll.getAbsolutePath() + " 移动至了 " + minDestFileAll
+											+ " NO.149 srcFileIteamTheSameAsAllDestFillIteam: 1 ");
+									System.out.println(
+											srcFile_iteam.getAbsolutePath() + " 移动至了 " + destFileAll.getAbsolutePath()
+													+ " NO.150 srcFileIteamTheSameAsAllDestFillIteam:1 ");
+									break;
+								} else {
+									// 否则将其再进行比较
+									if (srcFile_iteam.length() == destFileAll_iteam.length()) {
+										// 如果质量相同则将其移动至 theSameDestFileAll 同名文件质量相等 文件夹
+										moveFile(srcFile_iteam.getAbsolutePath(), theSameDestFileAll);
+										System.err
+												.println(srcFile_iteam.getAbsolutePath() + " 移动至了 " + theSameDestFileAll
+														+ " NO.155 srcFileIteamTheSameAsAllDestFillIteam:2 ");
+										break;
+									} else {
+										// 否则将其移动至 minDestFileAll 同名文件质量较小 文件夹
+										moveFile(srcFile_iteam.getAbsolutePath(), minDestFileAll);
+										System.out.println(srcFile_iteam.getAbsolutePath() + " 移动至了 " + minDestFileAll
+												+ " NO.159 srcFileIteamTheSameAsAllDestFillIteam:3 ");
+										break;
+									}
+								}
+							}
+
+						}
+					}
+
+					if (!srcFileIteamTheSameAsAllDestFillIteam) {// 无重复时 将 srcFile_iteam 直接 存入到 destFileAll 文件夹
+						moveFile(srcFile_iteam.getAbsolutePath(), destFileAll.getAbsolutePath());
+						System.out.println(srcFile_iteam.getAbsolutePath() + " 移动至了 " + destFileAll.getAbsolutePath()
+								+ " NO.163 srcFileIteamTheSameAsAllDestFillIteam:4 ");
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * <p>Title: moveFile</p>  
+	 * <p>
+	 *    Description: 移动文件
+	 * </p>  
+	 * <p>Copyright: Copyright (c) 2017</p>  
+	 * <p>Company: www.baidudu.com</p>  
+	 * @param src
+	 * @param dest  
+	 * @author xianxian
+	 * @date 2023年1月27日  
+	 * @version 1.0
+	 */
+	private static void moveFile(String src, String dest) {
+		File targetPart = new File(dest);
+		if (!targetPart.exists()) {
+			targetPart.mkdirs();// 创建此抽象路径名指定的目录。
+		}
+		// 以src的最后一个"\"取其下标 截取字符从 最后一个 "\" 到结束 得到 src 的文件全名 再将其 拼接到目标文件名中
+		dest = dest + File.separator + src.substring(src.lastIndexOf(File.separator) + 1, src.length());
+		try {
+			Files.move(Paths.get(src), Paths.get(dest), StandardCopyOption.ATOMIC_MOVE);
+			System.out.println("成功了");
+		} catch (IOException e) {
+			System.out.println("失败了，失败信息： " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
